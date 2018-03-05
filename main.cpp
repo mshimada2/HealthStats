a4d23a2fd01b42fe34d0b265719f8c503c132c6e
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-03-04 22:30:00 -0800

    Adds vectors for beats per minute, steps taken, and calories burned

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Class collaboration for learning how to use vectors
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <int> bpm;
+    vector <int> steps;
+    vector <int> calories;
+
+    return 0;
+}
